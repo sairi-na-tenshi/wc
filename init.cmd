41de74b4d18dd11e01389087ee45da56509845f0
tenshi <sairi.na.tenshi@gmail.com>
2010-10-17 20:47:10 +0400

    master branch in init.cmd

--- init.cmd
+++ init.cmd
@@ -1,1 +1,1 @@
-git init && git pull git://github.com/sairi-na-tenshi/wc.git master && git submodule init && git submodule update
+git init && git pull git://github.com/sairi-na-tenshi/wc.git master && git submodule init && git submodule update && git submodule foreach git checkout master
